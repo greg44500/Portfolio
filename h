6cceee2a20727dd66c9a7d5e9ea0d1260fd2b0d6
greg44500 <gregdevweb44500@gmail.com>
2024-11-11 11:42:33 +0100

    Ajout des effets de transitions entre les sections

---
+++ h
@@ -0,0 +1,23 @@
+[33mcommit ce9e1937add49adb8815b2ea05baa2be8966f737[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m
+Author: greg44500 <gregdevweb44500@gmail.com>
+Date:   Sun Nov 10 22:41:12 2024 +0100
+
+    first commit
+
+[33mcommit f02e3e1c55f2397d87a0d54418c1a4c82d525ccf[m
+Author: greg44500 <gregdevweb44500@gmail.com>
+Date:   Sun Nov 10 22:39:13 2024 +0100
+
+    first commit
+
+[33mcommit 5617530e7d63f1c569c88eba0f9cc80446c5beba[m
+Author: greg44500 <gregdevweb44500@gmail.com>
+Date:   Sun Nov 10 22:38:47 2024 +0100
+
+    first commit
+
+[33mcommit 3fa80b8989ba674450f09be86243ec3285fc17d1[m
+Author: greg44500 <gregdevweb44500@gmail.com>
+Date:   Sun Nov 10 22:33:22 2024 +0100
+
+    first commit
--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const section = document.querySelector("#formation");
-const button = document.querySelector(".button");
-console.log(button);
-console.log(section);
+const buttons = document.querySelectorAll('.nav-button')
+const sections = document.querySelectorAll('.section')
 
-button.addEventListener("click", handleAnimation); // 'click' en minuscules
+// Variable pour suivre le z-index le plus élevé
+let currentZIndex = 1;
 
-function handleAnimation() {
-  if (section.classList.contains("animate")) {
-    section.classList.remove("animate");
-  }else  
-     {
-        section.classList.add('animate')
-    }
+// Fonction pour afficher la section cible et appliquer la transition
+function showSection(target) {
+  const section = document.getElementById(target);
+  
+  // Augmente le z-index et applique l'effet
+  currentZIndex++;
+  section.style.zIndex = currentZIndex;
+  
+  // Applique les classes 'animate' et 'fade-in' pour la transition
+  sections.forEach(sec => sec.classList.remove('animate'));
+  section.classList.add('animate');
+}
+
+
+// Ajoute l'événement de clic à chaque bouton
+buttons.forEach(button => {
+  button.addEventListener('click', handleClick);
+});
+
+// Fonction de gestion des clics sur les boutons
+function handleClick(event) {
+  const target = event.currentTarget.getAttribute('data-target');
+  showSection(target);
   
 }
 
